c2cea0a725c767983e9cc0f2e593886d1d8e4cbb
Iñaki Abete <inakiabt@gmail.com>
2023-03-04 15:09:03 -0300

    Initial commit

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const {assetExts} = defaultConfig.resolver;
+  return {
+    ...defaultConfig,
+    resolver: {
+      ...defaultConfig.resolver,
+      // Add bin to assetExts
+      assetExts: [...assetExts, 'bin'],
+    }
+  };
+})();
